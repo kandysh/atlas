ceaaa5d71d0b06eba2e76183d782d3dd2b9f5806
kandysh <rajawat.k.029@gmail.com>
2026-01-15 22:13:43 +0530

    Require pino-pretty only in development

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,7 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await require("pino");
-    await require("pino-pretty");
+    if (process.env.NODE_ENV === "development") await require("pino-pretty");
     await require("next-logger");
   }
 }
